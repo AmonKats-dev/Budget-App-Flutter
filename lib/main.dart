39a07bda1a7dd67b0321f2ddb3f952e948b51713
AmonKats-dev <amonkats8@gmail.com>
2019-11-15 19:45:05 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,30 +8,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Roboto'),
-      home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Roboto'),
+        home: HomePage(),
+        routes: <String, WidgetBuilder>{
+          '/listOfDistricts': (context) => ListOfDistricts(),
+          '/listOfSubcounties': (context) => SubCounty(),
+          '/summary': (context) => PDFSummary(),
+        });
+  }
+}
+
+class PDFSummary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Budget Summary'),
+      ),
+      body: loadFromAsset(),
     );
+  }
+
+  PDFDocument pdfDocument;
+  loadFromAsset() async {
+    pdfDocument =
+        await PDFDocument.fromAsset('assets/short_summary_dzaipi.pdf');
   }
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    void _pushSaved() {
-      Navigator.of(context).push(
-        new MaterialPageRoute(builder: (context) {
-          return new Scaffold(
-            appBar: AppBar(
-              title: new Text('List of districts'),
-            ),
-            body: data(),
-            //body: new ListView(),
-          );
-        }),
-      );
-    }
-
     return new Scaffold(
       appBar: AppBar(
         title: Center(child: Text('UG Budget')),
@@ -66,7 +75,7 @@ class HomePage extends StatelessWidget {
             new SizedBox(
                 width: 300.0,
                 height: 60.0,
-                child: new RaisedButton(
+                child: new FlatButton(
                   child: Text(
                     'KNOW YOUR BUDGET',
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
@@ -74,11 +83,25 @@ class HomePage extends StatelessWidget {
                   color: Color(0xFF18D191),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10.0)),
-                  onPressed: _pushSaved,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/listOfDistricts');
+                  },
                 ))
           ],
         ),
       ),
+    );
+  }
+}
+
+class ListOfDistricts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Local Government'),
+      ),
+      body: data(),
     );
   }
 
@@ -125,18 +148,255 @@ class HomePage extends StatelessWidget {
               children: <Widget>[
                 Text(countries[index]),
                 Image(
-                  image: AssetImage('images/right_arrow.png'),
+                  image: AssetImage('images/next_arrow.png'),
                   color: Colors.grey,
                 ),
               ],
             ),
-            onTap: () {},
+            onTap: () {
+              Navigator.pushNamed(context, '/listOfSubcounties');
+            },
           ),
         );
       },
-      /*separatorBuilder: (context, index) {
-        return Divider();
-      },*/
     );
   }
 }
+
+class SubCounty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: const Text('Sub-County'),
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (BuildContext context, int index) => EntryItem(
+          data[index],
+        ),
+      ),
+    );
+  }
+}
+
+class Entry {
+  final String title;
+  final List<Entry> children;
+  Entry(this.title, [this.children = const <Entry>[]]);
+}
+
+final List<Entry> data = <Entry>[
+  Entry(
+    'Dzaipi',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Arinyapi',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Ukusijoni',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Adropi',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Ofua',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Ciforo',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Pacara',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Pakele',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Adjumani TC',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+  Entry(
+    'Itirikwa',
+    <Entry>[
+      Entry(
+        'FY 2018/19',
+        <Entry>[
+          Entry('Budget Summary'),
+          Entry('Health'),
+          Entry('Schools'),
+        ],
+      ),
+    ],
+  ),
+];
+
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+  final Entry entry;
+
+  BuildContext get context => null;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) {
+      return ListTile(
+        title: Text(root.title),
+        onTap: () {
+          Navigator.pushNamed(context, '/summary');
+        },
+      );
+    }
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map<Widget>(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return _buildTiles(entry);
+  }
+}
+
+/*class SubCounty2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub-County'),
+      ),
+      body: data(),
+    );
+  }
+
+  */ /*data() {
+    final counties = [
+      'Dzaipi',
+      'Arinyapi',
+      'Ukusijoni',
+      'Adropi',
+      'Ofua',
+      'Ciforo',
+      'Pacara',
+      'Pakele',
+      'Adjumani TC',
+      'Itirikwa',
+    ];
+
+    return ListView.builder(
+      itemCount: counties.length,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+              border:
+                  Border(bottom: BorderSide(color: Colors.grey, width: 1.0))),
+          child: ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(counties[index]),
+                Image(
+                  image: AssetImage('images/next_arrow.png'),
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+            onTap: () {
+              //Navigator.pushNamed(context, '/listOfSubcounties');
+            },
+          ),
+        );
+      },
+    );
+  }
+}*/
